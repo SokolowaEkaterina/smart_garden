f1195e1b8b4ea3a73c736e322b8bb42cbd5ad43d
SokolowaEkaterina <soko20wa.ek0ter1na@gmail.com>
2022-02-19 01:25:16 +0400

    header,main_screen,service

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
             $('.language').addClass('show');
         }
     });
+
+    if ( /*window.innerWidth > 600 &*/ window.innerWidth < 840 ) {
+        $('.menu_btn').click(function(){
+            if($('.menu_btn').hasClass('active')){
+                $('.menu_btn').removeClass('active');
+                $('.navigation').removeClass('slid_active');
+            }else{
+                $('.menu_btn').addClass('active');
+                $('.navigation').addClass('slid_active');
+            }
+        });
+    };
+
+    $(window).on('load resize', function() {
+        if ($(window).width() < 691) {
+          $('.services_block').slick({
+            centerMode: true,
+            arrows: false,
+            slidesToShow: 1,
+          });
+        } else {
+          $('.services_block').slick('unslick');
+        }
+      });
 });
